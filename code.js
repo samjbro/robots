42f0d89787751fd89c2a8a70ad26666426d25714
cookywook <cookywook@gmail.com>
2020-06-04 15:43:28 +0100

    Bunch of changes

--- code.js
+++ code.js
@@ -1,19 +1,23 @@
-// starting health
+// defining the bots
 var robot_1 = {
 	name:"Robot 1",
 	health:10,
+	wins:0,
 }
 
 var robot_2 = {
 	name:"Robot 2",
 	health:10,
+	wins:0,
 }
 
 var robots = [robot_1, robot_2];
+var opponent = 1;
 
 // get a random current robot
 var current_robot = robots[Math.floor(Math.random() * robots.length)];
 
+//function to switch bots during a game
 function botSwitch(x){
 	if (x == robot_1){
   current_robot = robot_2;
@@ -25,10 +29,12 @@ function botSwitch(x){
   }
 }
 
-// define starter cards
+// ACTIONS
+
+// attack does 1 damage to the other bot
 function attack(){
  if (current_robot == robot_1){
- 	robot_2.health--;
+	robot_2.health--;
   console.log('Robot 1 attacks!')
 	}
  else {
@@ -37,54 +43,112 @@ function attack(){
  }
 }
 
-
-
 //TO DO function move (){
 
-//}
-
+// repair heals 1 damage to the current bot
 function repair(){
-
+	if (current_robot == robot_1){
+  	// robot_1.health++;
+   console.log('Robot 1 attacks!')
+ 	}
+  else {
+  	// robot_2.health++;
+   console.log('Robot 2 attacks!')
+  }
 }
 
-// array of functions
-var deck = ["attack",]
+function shuffle(){
+	robot_1.deck = [attack,attack,attack,attack];
+	robot_2.deck = [attack,attack,attack,attack];
+	console.log('randomising!');
+	//randomising code...
+	robot_1.deck.sort(function(a, b){return 0.5 - Math.random()});
+	robot_2.deck.sort(function(a, b){return 0.5 - Math.random()});
+}
 
-// testing
+
+
+// CHOOSE: pick an ACTION from deck
+function choose(x){
+	console.log("@@@" + " " + x.name + " " + "choosing!");
+	x.deck[0]();
+	x.deck.shift();
+	console.log("remaining cards" + " " + x.deck.length);
+}
+
+
+// being loud for testing
 console.log("START");
 console.log("Starting bot is" + " " + current_robot.name);
-console.log("Robot 1 health =" + " " + robot_1.health);
-console.log("Robot 2 health =" + " " + robot_2.health);
+console.log("Robot 1 starting health:" + " " + robot_1.health);
+console.log("Robot 2 starting health:" + " " + robot_2.health);
 
-for (var round = 0; round < 100; round++){
+// RESETs health at the start of each game
+function reset(){
+	shuffle(robot_1);
+	shuffle(robot_2);
+	if (round = 1){
+		robot_2.health = 10;
+		robot_1.health = 10;
+	}
+	else {
+	botSwitch();
+	}
+}
+
+// SHUFFLE CHECK - checks to see if the deck needs a SHUFFLE
+function shuffleCheck(a){
+	console.log("robot 1 deck=" + " " + robot_1.deck.length);
+	console.log("robot 2 deck=" + " " + robot_2.deck.length);
+	if(a.length == 0){
+	 	console.log("SHUFFLING DECK!!!" + " " + "for" + " " + current_robot.name);
+	shuffle(a);
+	}
+}
+
+// selects a winner when a bot's health = 10
+// currently badly written assuming just 2p
+function pickWinner(){
+	if (robot_1.health > 0) {
+	winner = robot_1;
+	robot_1.wins++;
+	}
+ else {
+	winner = robot_2;
+	robot_2.wins++;
+	 }
+}
+
+//PLAY
+function play(){
+	var round = 1;
+	for (var round = 1; round < 100; round++){
 	console.log("***************Round" + " " + round + "******************");
-  //skip botswitch first round
-  if (round > 0) {
-  botSwitch(current_robot);
-  }
+	// choose active bot
+	botSwitch(current_robot);
   console.log("Current bot is" + " " + current_robot.name);
-	attack();
+	// pick first available action from deck
+	choose(current_robot);
+	// show updated stats
 	console.log("Robot 1 health =" + " " + robot_1.health);
 	console.log("Robot 2 health =" + " " + robot_2.health);
+	//check for shuffle
+	shuffleCheck(current_robot.deck);
+	//check for winnner
   if (robot_1.health == 0 |	robot_2.health == 0){
- 	 if (robot_1.health > 0) {
-   var winner = robot_1;
-   }
-  else {
-   var winner = robot_2;
-  	}
-  round = 100;
+	pickWinner();
+	round = 100;
 	console.log("The winner is...." + " " + winner.name);
   }
 }
+}
 
-// var a = confirm("Attack?");
-// if (a == true) {
-// 	attack();
-// }
-// else {
-// console.log("No attack");
-// }
-
-// console.log("Robot 1 health =" + " " + robot_1.health);
-// console.log("Robot 2 health =" + " " + robot_2.health);
+//PLAY MULTIPLE GAMES
+for (var games = 1; games < 100; games++){
+		console.log("New game!!!!!!!!!!!!!!!!!!!!!!!");
+		reset();
+ 		play();
+		console.log("Games =" + " " + games);
+		console.log('Robot 1 has won' + " " + robot_1.wins + " " + "games");
+		console.log('Robot 2 has won' + " " + robot_2.wins + " " + "games");
+}
